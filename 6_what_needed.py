8c5e430a345dd44ed35cddfe3432d0798d81da88
Acteonis <125166906+Acteonis@users.noreply.github.com>
2023-10-18 19:56:49 -0500

    Delete 6_what_needed.py

--- 6_what_needed.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-text = "There shoul be one-- and preferably only one --obvious way to do it. Ithough that way may be obvious at first unless you're Dutch."
-a = text[26:45]
-b = text[55:59]
-c = text[-21:-13]
-print(a+b+c)
